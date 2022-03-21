eb35c104d0b72a53357735f521fd2aed94643417
TimSon777 <dusalimov.tima@mail.ru>
2022-03-21 21:43:05 +0300

    ADD: TrackController

---
+++ src/LiWiMus.Web/Controllers/TrackController.cs
@@ -0,0 +1,40 @@
+﻿using LiWiMus.Core.Entities;
+using LiWiMus.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiWiMus.Web.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class TrackController : ControllerBase
+{
+    private readonly IRepository<Track> _trackRepository;
+    private readonly IWebHostEnvironment _environment;
+
+    public TrackController(IRepository<Track> trackRepository, IWebHostEnvironment environment)
+    {
+        _trackRepository = trackRepository;
+        _environment = environment;
+    }
+    
+    [HttpGet]
+    [Route("{trackId:int}")]
+    public async Task<IActionResult> Get(int trackId)
+    {
+        var track = await _trackRepository.GetByIdAsync(trackId);
+        
+        if (track is null)
+        {
+            return BadRequest();
+        }
+
+        var pathToTrack = Path.Combine(_environment.ContentRootPath, track.PathToFile);
+
+        if (!System.IO.File.Exists(pathToTrack))
+        {
+            return BadRequest();
+        }
+        
+        return PhysicalFile(pathToTrack, "audio/mpeg", true);
+    }
+}
