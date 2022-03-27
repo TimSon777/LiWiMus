d4b33ea3b6f42e6f0038d171422f0c765841d393
LipatovAlexander <lipatov.work@bk.ru>
2022-03-27 22:59:34 +0300

    ADD: Avatar cropper

--- src/LiWiMus.Web/wwwroot/js/update_profile.js
+++ src/LiWiMus.Web/wwwroot/js/update_profile.js
@@ -6,6 +6,14 @@ failureUpdate = function (xhr) {
     alert(`Status code ${xhr.status}, message ${xhr.responseText}`);
 }
 
+var avatarBlob;
+
+beforeSubmit = function (rr, $form) {
+    if (avatarBlob) {
+        $form.data.append('Avatar', avatarBlob, 'avatar.jpg');
+    }
+}
+
 $(document).ready(() => {
     let isShowSaveBtn = false;
     let saveBtn = $("#btn-save-profile-info");
@@ -24,5 +32,63 @@ $(document).ready(() => {
     
     $("input[name='gender']").change(function () {
         $('#is-male').val(this.id.indexOf("female") === -1);
+    });
+
+    var avatar = document.getElementById('avatar');
+    var image = document.getElementById('image');
+    var input = document.getElementById('input');
+    var $modal = $('#modal');
+    var cropper;
+
+    input.addEventListener('change', function (e) {
+        var files = e.target.files;
+        var done = function (url) {
+            input.value = '';
+            image.src = url;
+            $modal.modal('show');
+        };
+        var reader;
+        var file;
+
+        if (files && files.length > 0) {
+            file = files[0];
+
+            if (URL) {
+                done(URL.createObjectURL(file));
+            } else if (FileReader) {
+                reader = new FileReader();
+                reader.onload = function (e) {
+                    done(reader.result);
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+
+    $modal.on('shown.bs.modal', function () {
+        cropper = new Cropper(image, {
+            aspectRatio: 1,
+            viewMode: 3,
+        });
+    }).on('hidden.bs.modal', function () {
+        cropper.destroy();
+        cropper = null;
+    });
+
+    document.getElementById('crop').addEventListener('click', function () {
+        var canvas;
+
+        $modal.modal('hide');
+
+        if (cropper) {
+            canvas = cropper.getCroppedCanvas({
+                width: 300,
+                height: 300,
+            });
+            avatar.src = canvas.toDataURL();
+            canvas.toBlob(function (blob) {
+                avatarBlob = blob;
+            });
+        }
     });
 })
