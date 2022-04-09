ce0ab2b0a275aafd5f6e7bae1d9c0077bdb337fb
TimSon777 <dusalimov.tima@mail.ru>
2022-04-09 20:57:07 +0300

    FIX: bugs

--- src/LiWiMus.Admin/server/src/app.module.ts
+++ src/LiWiMus.Admin/server/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from 'path'
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(), 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/admin'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
