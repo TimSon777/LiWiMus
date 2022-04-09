6091f9acbb77daee1da7e3714e6cf9bbb9e580c7
LipatovAlexander <lipatov.work@bk.ru>
2022-04-10 00:08:01 +0300

    🍻 ADD: ormconfig.js to use environment variables

---
+++ src/LiWiMus.Admin/server/ormconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: true,
+  entities: ['dist/**/*.js'],
+};
