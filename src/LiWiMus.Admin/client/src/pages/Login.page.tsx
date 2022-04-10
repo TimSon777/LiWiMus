3f0dedbecdb1d3453e72e263b608063ed09f2bef
LipatovAlexander <lipatov.work@bk.ru>
2022-04-11 00:59:37 +0300

    💩 FIX: center form on mobile

--- src/LiWiMus.Admin/client/src/pages/Login.page.tsx
+++ src/LiWiMus.Admin/client/src/pages/Login.page.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Button, Grid, Stack } from "@mui/material";
+import { Box, Button, Grid, Stack } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { AuthContext } from "../contexts/Auth.context";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -26,48 +26,50 @@ export default function LoginPage() {
   };
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ height: "100%" }}
-      spacing={2}
-    >
-      <Grid item xs="auto" lg={2}>
-        <Stack spacing={2}>
-          <ContrastTextField
-            error={!!errors.username && !!errors.username.message}
-            label="Username"
-            variant="outlined"
-            fullWidth
-            {...register("username", {
-              required: { value: true, message: "Enter username" },
-            })}
-            helperText={errors.username?.message}
-          />
-          <ContrastTextField
-            error={!!errors.password && !!errors.password.message}
-            label="Password"
-            variant="outlined"
-            type="password"
-            {...register("password", {
-              required: { value: true, message: "Enter password" },
-            })}
-            helperText={errors.password?.message}
-          />
-        </Stack>
+    <Box sx={{ position: "relative", height: "100%" }}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{ position: "absolute", top: "50%", transform: "translateY(-50%)" }}
+      >
+        <Grid item xs="auto" lg={2}>
+          <Stack spacing={2}>
+            <ContrastTextField
+              error={!!errors.username && !!errors.username.message}
+              label="Username"
+              variant="outlined"
+              fullWidth
+              {...register("username", {
+                required: { value: true, message: "Enter username" },
+              })}
+              helperText={errors.username?.message}
+            />
+            <ContrastTextField
+              error={!!errors.password && !!errors.password.message}
+              label="Password"
+              variant="outlined"
+              type="password"
+              {...register("password", {
+                required: { value: true, message: "Enter password" },
+              })}
+              helperText={errors.password?.message}
+            />
+          </Stack>
+        </Grid>
+        <Grid item xs="auto">
+          <Button
+            variant="contained"
+            endIcon={<ArrowForwardIcon />}
+            sx={{ borderRadius: "20px", px: 4 }}
+            onClick={handleSubmit(loginHandler)}
+          >
+            Next
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs="auto">
-        <Button
-          variant="contained"
-          endIcon={<ArrowForwardIcon />}
-          sx={{ borderRadius: "20px", px: 4 }}
-          onClick={handleSubmit(loginHandler)}
-        >
-          Next
-        </Button>
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
