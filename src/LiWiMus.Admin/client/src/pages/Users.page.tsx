812f978759782d1a827f3a7dfac82662fa8eb934
klkokou <darya26zapuskalova0202@gmail.com>
2022-04-12 22:54:35 +0300

    refactoring users page

--- src/LiWiMus.Admin/client/src/pages/Users.page.tsx
+++ src/LiWiMus.Admin/client/src/pages/Users.page.tsx
@@ -1,5 +1,15 @@
 import React, {useState, useEffect} from "react";
-import {DataGrid, GridRowsProp, GridColDef} from '@mui/x-data-grid'
+import {
+    DataGrid,
+    GridRowsProp,
+    GridColDef,
+    GridApi,
+    GridCellValue,
+    GridToolbar,
+    GridToolbarContainer
+} from '@mui/x-data-grid';
+import {Button} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export default function UsersPage() {
     const [users, setUsers] = useState([])
@@ -15,20 +25,47 @@ export default function UsersPage() {
 
     const rows: GridRowsProp = users;
     const columns: GridColDef[] = [{field: 'id', hide: true}, {
-        field: 'username', headerName: 'Username', flex:1
-    },{
-        field: 'email', headerName: 'Email', flex:1
-    },  {
-        field: 'phoneNumber', headerName: 'Phone Number', flex:1
+        field: 'username', headerName: 'Username', flex: 0.6
     }, {
-        field: 'enum', headerName: 'Gender', flex:1
+        field: 'email', headerName: 'Email', flex: 0.7
     }, {
-        field: 'emailConfirmed', headerName: 'Email Confirmed', flex:1
+        field: 'phoneNumber', headerName: 'Phone Number', flex: 0.5
     }, {
-        field: 'phoneConfirmed', headerName: 'Phone Confirmed',
+        field: 'enum', headerName: 'Gender', flex: 0.3
     }, {
-        field: 'balance', headerName: 'Balance', 
-    }];
+        field: 'emailConfirmed', headerName: 'Email Confirmed', flex: 0.4, type: "boolean"
+    }, {
+        field: 'phoneConfirmed', headerName: 'Phone Confirmed', flex: 0.4, type: "boolean"
+    }, {
+        field: 'balance', headerName: 'Balance', flex: 0.2
+    }, {
+        field: 'action',
+        headerName: 'Edit',
+        sortable: false,
+        flex: 0.5,
+        filterable: false,
+        renderCell: (params) => {
+            const onClick = (e: any) => {
+                e.stopPropagation();
+                const api: GridApi = params.api;
+                const fields = api
+                    .getAllColumns()
+                    .map((c) => c.field)
+                    .filter((c) => c !== "__check__" && !!c);
+                const thisRow: any = {};
+
+                fields.forEach((f) => {
+                    thisRow[f] = params.getValue(params.id, f);
+                });
+
+                return alert(JSON.stringify(thisRow, null, 4));
+            };
+
+            return <Button variant="contained"
+                           endIcon={<ArrowForwardIcon/>}
+                           sx={{borderRadius: "20px", px: 4}} onClick={onClick}>Edit</Button>;
+        },
+    },];
 
     return (
         <div>
@@ -36,7 +73,13 @@ export default function UsersPage() {
                 <h1>Users page</h1>
             </div>
             <div>
-                <DataGrid columns={columns} rows={rows} pageSize={8} checkboxSelection={true} autoHeight></DataGrid>
+                <DataGrid
+                    columns={columns}
+                    rows={rows}
+                    components={{Toolbar: GridToolbar}}
+                    pageSize={5}
+                    checkboxSelection={true}
+                    autoHeight></DataGrid>
             </div>
         </div>
     );
