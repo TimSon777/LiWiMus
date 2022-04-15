5813cf96cb800ba5c087d3a4c3411e4b1e5824ca
LipatovAlexander <lipatov.work@bk.ru>
2022-04-15 22:16:51 +0300

    🍻 FIX: Payment

--- src/LiWiMus.Web/Areas/User/Controllers/PaymentController.cs
+++ src/LiWiMus.Web/Areas/User/Controllers/PaymentController.cs
@@ -1,6 +1,10 @@
 ﻿using FormHelper;
 using LiWiMus.Core.Exceptions;
+using LiWiMus.Core.IdentityAggregates;
+using LiWiMus.Core.IdentityAggregates.Specifications;
 using LiWiMus.Core.Interfaces;
+using LiWiMus.Infrastructure.Identity;
+using LiWiMus.SharedKernel.Interfaces;
 using LiWiMus.Web.Areas.User.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,12 +15,14 @@ namespace LiWiMus.Web.Areas.User.Controllers;
 public class PaymentController : Controller
 {
     private readonly IPaymentService _paymentService;
-    private readonly UserManager<Core.Users.User> _userManager;
+    private readonly UserManager<UserIdentity> _userManager;
+    private readonly IRepository<IdentityAggregate> _aggregateRepository;
 
-    public PaymentController(IPaymentService paymentService, UserManager<Core.Users.User> userManager)
+    public PaymentController(IPaymentService paymentService, UserManager<UserIdentity> userManager, IRepository<IdentityAggregate> aggregateRepository)
     {
         _paymentService = paymentService;
         _userManager = userManager;
+        _aggregateRepository = aggregateRepository;
     }
 
     public IActionResult Index()
@@ -40,7 +46,19 @@ public class PaymentController : Controller
     [FormValidator]
     public async Task<IActionResult> Pay(PaymentViewModel model)
     {
-        var user = await _userManager.GetUserAsync(User);
+        var identity = await _userManager.GetUserAsync(User);
+        var aggregate = await _aggregateRepository.GetBySpecAsync(new IdentityAggregateByIdWithUserSpec(identity.Id));
+        if (aggregate is null)
+        {
+            return BadRequest();
+        }
+
+        var user = aggregate.User;
+        if (user is null)
+        {
+            return BadRequest();
+        }
+
         try
         {
             await _paymentService.PayAsync(user, model.Amount, model.Reason);
