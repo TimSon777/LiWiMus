bdb7be160bede9a0ed33df28c2d7c2e1d3a3bde0
LipatovAlexander <lipatov.work@bk.ru>
2022-04-19 17:43:13 +0300

    ♻️ REFACTOR: Get permissions using reflection

--- src/LiWiMus.Core/Permissions/DefaultPermissions.cs
+++ src/LiWiMus.Core/Permissions/DefaultPermissions.cs
@@ -1,118 +1,96 @@
-﻿namespace LiWiMus.Core.Permissions;
+﻿using System.Reflection;
+
+namespace LiWiMus.Core.Permissions;
 
 public static class DefaultPermissions
 {
+    private static List<string>? _allPermissions;
+
     public static List<string> GetAllPermissions()
     {
-        return new List<string>
+        if (_allPermissions is not null)
         {
-            Artist.Create,
-            Artist.Read,
-            Artist.Update,
-            Artist.Delete,
+            return _allPermissions;
+        }
 
-            Album.Create,
-            Album.Read,
-            Album.Update,
-            Album.Delete,
+        _allPermissions = new List<string>();
+        var type = typeof(DefaultPermissions);
+        var nestedTypes = type.GetNestedTypes();
+        foreach (var nestedType in nestedTypes)
+        {
+            var constants =
+                nestedType.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                          .Where(fi => fi.IsLiteral && !fi.IsInitOnly)
+                          .Select(x => x.GetRawConstantValue() as string)
+                          .Where(x => !string.IsNullOrEmpty(x));
+            _allPermissions.AddRange(constants!);
+        }
 
-            Genre.Create,
-            Genre.Read,
-            Genre.Update,
-            Genre.Delete,
-
-            Playlist.Create,
-            Playlist.Read,
-            Playlist.Update,
-            Playlist.Delete,
-
-            Role.Create,
-            Role.Read,
-            Role.Update,
-            Role.Delete,
-
-            Track.Create,
-            Track.Read,
-            Track.Update,
-            Track.Delete,
-
-            User.Create,
-            User.Read,
-            User.Update,
-            User.Delete,
-
-            Transaction.Create,
-            Transaction.Read,
-            Transaction.Update,
-            Transaction.Delete,
-
-            Chat.Ask,
-            Chat.Answer
-        };
+        return _allPermissions;
     }
 
     public static class Artist
     {
-        public const string Create  = $"{nameof(Artist)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Artist)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Artist)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Artist)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Artist)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Artist)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Artist)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Artist)}.{nameof(Delete)}";
     }
 
     public static class Album
     {
-        public const string Create  = $"{nameof(Album)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Album)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Album)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Album)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Album)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Album)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Album)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Album)}.{nameof(Delete)}";
     }
 
     public static class Genre
     {
-        public const string Create  = $"{nameof(Genre)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Genre)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Genre)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Genre)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Genre)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Genre)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Genre)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Genre)}.{nameof(Delete)}";
     }
 
     public static class Playlist
     {
-        public const string Create  = $"{nameof(Playlist)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Playlist)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Playlist)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Playlist)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Playlist)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Playlist)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Playlist)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Playlist)}.{nameof(Delete)}";
     }
 
     public static class Role
     {
-        public const string Create  = $"{nameof(Role)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Role)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Role)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Role)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Role)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Role)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Role)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Role)}.{nameof(Delete)}";
     }
 
     public static class Track
     {
-        public const string Create  = $"{nameof(Track)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Track)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Track)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Track)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Track)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Track)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Track)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Track)}.{nameof(Delete)}";
     }
 
     public static class User
     {
-        public const string Create  = $"{nameof(User)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(User)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(User)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(User)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(User)}.{nameof(Create)}";
+        public const string Read = $"{nameof(User)}.{nameof(Read)}";
+        public const string Update = $"{nameof(User)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(User)}.{nameof(Delete)}";
     }
 
     public static class Transaction
     {
-        public const string Create  = $"{nameof(Transaction)}.{nameof(Create)}";
-        public const string Read    = $"{nameof(Transaction)}.{nameof(Read)}";
-        public const string Update  = $"{nameof(Transaction)}.{nameof(Update)}";
-        public const string Delete  = $"{nameof(Transaction)}.{nameof(Delete)}";
+        public const string Create = $"{nameof(Transaction)}.{nameof(Create)}";
+        public const string Read = $"{nameof(Transaction)}.{nameof(Read)}";
+        public const string Update = $"{nameof(Transaction)}.{nameof(Update)}";
+        public const string Delete = $"{nameof(Transaction)}.{nameof(Delete)}";
     }
 
     public static class Chat
