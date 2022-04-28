9805a74864512c8d5788de2e5f315df91d177f57
klkokou <darya26zapuskalova0202@gmail.com>
2022-04-28 20:30:58 +0300

    FIX: artist profile

---
+++ src/LiWiMus.Web/Areas/Artist/Views/Tracks/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LiWiMus.Web.Areas.Artist.Views.Tracks;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
