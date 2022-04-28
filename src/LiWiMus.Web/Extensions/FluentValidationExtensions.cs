ce658d8053379767fe82f1ff4560a5aede47d4be
Lipatov Alexander <lipatov.work@bk.ru>
2022-04-28 04:14:27 +0300

    :bug: fix(web): Image validation bug
    Fix a bug emerging when use MaxSize or MaximumDifferent... with NotNull. Not sure why, but there was an NullReferenceException. "When(... is not null)" also doesn't help. Hope to rewrite it better later.

--- src/LiWiMus.Web/Extensions/FluentValidationExtensions.cs
+++ src/LiWiMus.Web/Extensions/FluentValidationExtensions.cs
@@ -13,8 +13,8 @@ public static class FluentValidationExtensions
         this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder.Matches(RegularExpressions.DisableTags);
 
-    public static IRuleBuilderOptions<T, ImageInfo?> MaximumDifferenceSidesInPercent<T>(
-        this IRuleBuilder<T, ImageInfo?> ruleBuilder, int maximumAspectRatioPercentage)
+    public static IRuleBuilderOptions<T, ImageInfo> MaximumDifferenceSidesInPercent<T>(
+        this IRuleBuilder<T, ImageInfo> ruleBuilder, int maximumAspectRatioPercentage)
     {
         if (maximumAspectRatioPercentage is < 0 or > 100)
         {
@@ -22,19 +22,33 @@ public static class FluentValidationExtensions
         }
 
         return ruleBuilder.Must(imageInfo =>
-            {
-                var image = imageInfo!.Image;
-                var width = image.Width;
-                var height = image.Height;
-                return Math.Abs(width - height) / (double) Math.Max(width, height) * 100 <= maximumAspectRatioPercentage;
-            })
-            .WithMessage($"Image width and height must differ by no more than {maximumAspectRatioPercentage} percent");
+                          {
+                              if (imageInfo is null)
+                              {
+                                  return true;
+                              }
+                              var image = imageInfo.Image;
+                              var width = image.Width;
+                              var height = image.Height;
+                              return Math.Abs(width - height) / (double) Math.Max(width, height) * 100 <=
+                                     maximumAspectRatioPercentage;
+                          })
+                          .WithMessage(
+                              $"Image width and height must differ by no more than {maximumAspectRatioPercentage} percent");
     }
 
     public static IRuleBuilderOptions<T, ImageInfo?> MaxSize<T>(
         this IRuleBuilder<T, ImageInfo?> ruleBuilder, ByteSize byteSize)
     {
-        return ruleBuilder.Must(image => image!.ByteSize <= byteSize)
-            .WithMessage($"The image must weigh less than {byteSize.ToString()}");
+        return ruleBuilder.Must(image =>
+                          {
+                              if (image is null)
+                              {
+                                  return true;
+                              }
+
+                              return image.ByteSize <= byteSize;
+                          })
+                          .WithMessage($"The image must weigh less than {byteSize.ToString()}");
     }
 }
