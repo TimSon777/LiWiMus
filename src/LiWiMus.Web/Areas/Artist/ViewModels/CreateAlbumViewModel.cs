ef008cc1ef996d5ba894b430ba32ee93648b3396
Lipatov Alexander <lipatov.work@bk.ru>
2022-04-28 06:47:00 +0300

    :sparkles: feat(web): Album: CRUD
    Add AlbumsController in Artist area with basic actions:
    1. /artist/{artistId}/albums - get albums associated with given artistId
    2. /artist/{artistId}/albums/create - create new album and associate with given artistId
    3. /artist/{artistId}/albums/{id} - get/edit/delete album by given id
    
    Use FormHelper for better ajax requests

---
+++ src/LiWiMus.Web/Areas/Artist/ViewModels/CreateAlbumViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LiWiMus.Core.Models;
+
+namespace LiWiMus.Web.Areas.Artist.ViewModels;
+
+public class CreateAlbumViewModel
+{
+    public string Title { get; set; } = null!;
+    public ImageInfo Cover { get; set; } = null!;
+    public DateOnly PublishedAt { get; set; }
+}
