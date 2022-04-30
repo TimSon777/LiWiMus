ae0ff2a80698ea8bd09b373ad1501e43c8816a52
Lipatov Alexander <lipatov.work@bk.ru>
2022-04-30 22:59:22 +0300

    :sparkles: feat(api): Add "Update playlist" endpoint

---
+++ src/LiWiMus.API/Playlists/MapProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LiWiMus.API.Playlists.Update;
+using LiWiMus.Core.Playlists;
+
+namespace LiWiMus.API.Playlists;
+
+public class MapProfile : Profile
+{
+    public MapProfile()
+    {
+        CreateMap<Request, Playlist>();
+    }
+}
---
+++ src/LiWiMus.API/Playlists/Update/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace LiWiMus.API.Playlists.Update;
+
+public class Request
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public bool IsPublic { get; set; }
+    public string PhotoBase64 { get; set; } = null!;
+}
