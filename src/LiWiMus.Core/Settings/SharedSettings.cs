0061ea9cd1bc5110bc7bc0f714e9b2671aa4256f
Lipatov Alexander <lipatov.work@bk.ru>
2022-05-06 00:20:19 +0300

    :bug: fix(web): Create data directories on startup
    Fix DirectoryNotFoundException

--- src/LiWiMus.Core/Settings/SharedSettings.cs
+++ src/LiWiMus.Core/Settings/SharedSettings.cs
@@ -4,4 +4,10 @@ public class SharedSettings
 {
     public DataSettings DataSettings { get; set; } = null!;
     public string SharedDirectory { get; set; } = null!;
+
+    public void CreateDirectories()
+    {
+        Directory.CreateDirectory(Path.Combine(SharedDirectory, DataSettings.MusicDirectory));
+        Directory.CreateDirectory(Path.Combine(SharedDirectory, DataSettings.PicturesDirectory));
+    }
 }
