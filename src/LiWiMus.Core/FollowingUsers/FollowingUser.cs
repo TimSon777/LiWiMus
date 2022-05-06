81fc3286b55632dfeb380cf853366c6b1084bbad
Lipatov Alexander <lipatov.work@bk.ru>
2022-05-07 00:27:56 +0300

    :recycle: refactor: Rename LikedUser to FollowingUser
    Rename classes, files
    Add database migration

---
+++ src/LiWiMus.Core/FollowingUsers/FollowingUser.cs
@@ -0,0 +1,10 @@
+﻿namespace LiWiMus.Core.FollowingUsers;
+
+public class FollowingUser : BaseEntity
+{
+    public virtual User Follower { get; set; } = null!;
+    public virtual User Following { get; set; } = null!;
+
+    public int FollowerId { get; set; }
+    public int FollowingId { get; set; }
+}
--- src/LiWiMus.Core/LikedUsers/LikedUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LiWiMus.Core.LikedUsers;
-
-public class LikedUser : BaseEntity
-{
-    public User User { get; set; } = null!;
-    public User Liked { get; set; } = null!;
-}
