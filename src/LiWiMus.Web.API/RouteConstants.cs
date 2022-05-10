1de211c8637be18cc8a4631825ce2d39bf44d27a
TimSon777 <dusalimov.tima@mail.ru>
2022-05-10 21:30:34 +0300

    refactor(Web.Admin.API): Add RouteConstants

---
+++ src/LiWiMus.Web.API/RouteConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace LiWiMus.Web.API;
+
+public static class RouteConstants
+{
+    private const string Prefix = "/api";
+    
+    public static class Albums
+    {
+        public const string Create = $"{Prefix}/{nameof(Albums)}";
+        public const string Read = $"{Prefix}/{nameof(Albums)}/{{id:int}}";
+        public const string Delete = $"{Prefix}/{nameof(Albums)}/{{id:int}}";
+        public const string Update = $"{Prefix}/{nameof(Albums)}";
+        //public const string ReadList = $"{Prefix}/{nameof(Albums)}";
+    }
+}
