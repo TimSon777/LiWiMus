b04de727a8afcb399d98febc1e684c261aa7c0dd
TimSon777 <dusalimov.tima@mail.ru>
2022-05-13 16:55:58 +0300

    feature(Web.Admin.API/NestJs): Add swagger

--- src/LiWiMus.Web.Admin/server/src/main.ts
+++ src/LiWiMus.Web.Admin/server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('/api');
+  
+  const config = new DocumentBuilder()
+      .setTitle("LiWiMus.Admin.Web.API.Nest")
+      .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3001);
 }
 bootstrap();
