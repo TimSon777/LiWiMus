917cff5ca887333ecd988360043bbfbcf23c9e59
TimSon777 <dusalimov.tima@mail.ru>
2022-05-15 22:41:16 +0300

    fix(Auth): Update cors

--- src/LiWiMus.Web.Auth/Program.fs
+++ src/LiWiMus.Web.Auth/Program.fs
@@ -90,9 +90,9 @@ module Program =
 
         app
             .UseRouting()
+            .UseCors()
             .UseAuthentication()
             .UseAuthorization()
-            .UseCors()
 
         app.UseEndpoints (fun endpoints ->
             endpoints.MapControllerRoute("default", "/auth/{controller}/{action}/{id?}")
