59dbf0d885f2cb0c73328f23969e74f5e698a95c
Lipatov Alexander <lipatov.work@bk.ru>
2022-05-20 07:52:43 +0300

    :recycle: refactor(web/admin/api): Explicitly include instead of lazy

--- src/LiWiMus.Web.API/Playlists/Tracks/List/Endpoint.cs
+++ src/LiWiMus.Web.API/Playlists/Tracks/List/Endpoint.cs
@@ -2,6 +2,7 @@
 using FluentValidation;
 using LiWiMus.Core.Playlists;
 using LiWiMus.Core.Tracks;
+using LiWiMus.Core.Tracks.Specifications;
 using LiWiMus.SharedKernel.Interfaces;
 using LiWiMus.Web.API.Shared;
 using LiWiMus.Web.API.Shared.Extensions;
@@ -14,7 +15,8 @@ namespace LiWiMus.Web.API.Playlists.Tracks.List;
 
 public class Endpoint : IEndpoint<IResult, Request>
 {
-    private IRepository<Playlist> _repository = null!;
+    private IRepository<Playlist> _playlistRepository = null!;
+    private IRepository<Track> _trackRepository = null!;
     private readonly IValidator<Request> _validator;
     private readonly IMapper _mapper;
 
@@ -32,30 +34,28 @@ public class Endpoint : IEndpoint<IResult, Request>
             return Results.ValidationProblem(validationResult.ToDictionary());
         }
 
-        var playlist = await _repository.GetByIdAsync(request.PlaylistId);
+        var playlist = await _playlistRepository.GetByIdAsync(request.PlaylistId);
         if (playlist is null)
         {
             return Results.Extensions.NotFoundById(EntityType.Playlists, request.PlaylistId);
         }
 
-        var tracks = playlist.PlaylistTracks
-                             .OrderBy(pt => pt.CreatedAt)
-                             .Skip((request.Page - 1) * request.ItemsPerPage)
-                             .Take(request.ItemsPerPage)
-                             .Select(pt => pt.Track)
-                             .ToList();
+        var tracks = await _trackRepository.ListInPlaylistAsync(playlist, request.Page, request.ItemsPerPage);
+        var count = await _trackRepository.CountInPlaylistAsync(playlist);
 
         var trackDtos = _mapper.MapList<Track, TrackDto>(tracks);
-        var result = new PaginatedData<TrackDto>(request.Page, request.ItemsPerPage, playlist.Tracks.Count, trackDtos);
+        var result = new PaginatedData<TrackDto>(request.Page, request.ItemsPerPage, count, trackDtos);
         return Results.Ok(result);
     }
 
     public void AddRoute(IEndpointRouteBuilder app)
     {
         app.MapGet(RouteConstants.Playlists.Tracks.List,
-            async (int playlistId, int page, int itemsPerPage, IRepository<Playlist> repository) =>
+            async (int playlistId, int page, int itemsPerPage, IRepository<Playlist> playlistRepository,
+                   IRepository<Track> trackRepository) =>
             {
-                _repository = repository;
+                _playlistRepository = playlistRepository;
+                _trackRepository = trackRepository;
                 return await HandleAsync(new Request(playlistId, page, itemsPerPage));
             });
     }
