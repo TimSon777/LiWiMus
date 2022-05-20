72619ca6145ccfe4966cf4b7250de9c2428c12fb
Lipatov Alexander <lipatov.work@bk.ru>
2022-05-21 01:29:41 +0300

    :sparkles: feature(web/admin/client): Add feature to create artists

--- src/LiWiMus.Web.Admin/client/src/artists/Artist.service.ts
+++ src/LiWiMus.Web.Admin/client/src/artists/Artist.service.ts
@@ -2,6 +2,7 @@ import { FilterOptions } from "../shared/types/FilterOptions";
 import { Artist } from "./types/Artist";
 import axios from "../shared/services/Axios";
 import { PaginatedData } from "../shared/types/PaginatedData";
+import { CreateArtistDto } from "./types/CreateArtistDto";
 
 const ArtistService = {
   getArtists: async (options: FilterOptions<Artist>) => {
@@ -9,6 +10,11 @@ const ArtistService = {
       params: options,
     });
     return response.data as PaginatedData<Artist>;
+  },
+
+  save: async (artist: CreateArtistDto) => {
+    const response = await axios.post(`/artists`, artist);
+    return response.data as Artist;
   },
 };
 
