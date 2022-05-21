812304d62b214641a786cc6e41f8e92699d55001
LandyshH <86878681+LandyshH@users.noreply.github.com>
2022-05-21 16:31:09 +0300

    REFACTORING: save duration and file location for track

--- src/LiWiMus.Web.Admin/server/src/tracks/dto/track.dto.ts
+++ src/LiWiMus.Web.Admin/server/src/tracks/dto/track.dto.ts
@@ -6,7 +6,7 @@ import {PlaylistTrack} from "../../playlistTracks/playlistTrack.entity";
 import {Exclude, Expose} from "class-transformer";
 import {Album} from "../../albums/album.entity";
 import {GenreDto} from "../../genres/dto/genre.dto";
-import {IsDateString, IsNotEmpty, IsString, MaxLength, ValidateNested} from "class-validator";
+import {IsDateString, IsNotEmpty, IsNumber, IsString, MaxLength, ValidateNested} from "class-validator";
 import {ApiProperty} from "@nestjs/swagger";
 import {ArtistsDto} from "../../artists/dto/artists.dto";
 import {AlbumDto} from "../../albums/dto/album.dto";
--- src/LiWiMus.Web.Admin/server/src/tracks/dto/update.track.dto.ts
+++ src/LiWiMus.Web.Admin/server/src/tracks/dto/update.track.dto.ts
@@ -4,7 +4,7 @@ import {ApiProperty} from "@nestjs/swagger";
 import {GenreDto} from "../../genres/dto/genre.dto";
 import {AlbumDto} from "../../albums/dto/album.dto";
 import {ArtistsDto} from "../../artists/dto/artists.dto";
-import {IsArray, IsDateString, IsInt, IsString, MaxLength} from "class-validator";
+import {IsArray, IsDateString, IsInt, IsNumber, IsString, MaxLength} from "class-validator";
 
 @Exclude()
 export class UpdateTrackDto extends IdDto {
@@ -16,6 +16,7 @@ export class UpdateTrackDto extends IdDto {
 
     @ApiProperty()
     @Expose()
+    @IsNumber()
     duration: number;
 
     @ApiProperty()
@@ -25,5 +26,6 @@ export class UpdateTrackDto extends IdDto {
 
     @ApiProperty()
     @Expose()
+    @IsString()
     fileLocation: string;
 }
