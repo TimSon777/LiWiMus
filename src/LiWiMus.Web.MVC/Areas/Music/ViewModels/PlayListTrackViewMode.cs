bf5ba24633ffe4c1969f8014489445a836780215
TimSon777 <dusalimov.tima@mail.ru>
2022-05-29 01:28:17 +0300

    fix(MVC): Playlist page, add spec

--- src/LiWiMus.Web.MVC/Areas/Music/ViewModels/PlayListTrackViewMode.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LiWiMus.Web.MVC.Areas.Music.ViewModels;
-
-public class PlayListTrackViewMode
-{
-    public PlaylistViewModel Playlist { get; set; }
-    public TrackForPlaylistViewModel Track { get; set; }
-}
